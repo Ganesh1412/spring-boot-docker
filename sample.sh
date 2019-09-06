98e5b6f4f515f6ee5cb989b8afa8e5071fd5574c
Ganesh Babu <ganeshbabu@Ganeshs-MacBook-Pro.local>
2019-09-06 19:30:09 +0100

    Added New modules

--- sample.sh
+++
@@ -1,1 +0,0 @@
-java -jar /Users/ganeshbabu/spring-boot-docker/build/libs/spring-boot-duck-0.1.0.jar
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -4,16 +4,29 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hi there! I'm being served from, %s!";
     private final AtomicLong counter = new AtomicLong();
+    InetAddress ip;
+    String hostname;
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+	try {
+		ip = InetAddress.getLocalHost();
+		hostname = ip.getHostName();
+        	return new Greeting(counter.incrementAndGet(),
+                            String.format(template,hostname));
     }
+	catch(UnknownHostException e){
+		e.printStackTrace();
+		return new Greeting(0,String.format(template,"UnknownHost"));
+	}
+	
+	}
 }
